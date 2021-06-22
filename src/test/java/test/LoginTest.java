4a84a83092276905cb7bc7ffdf29ac206fa45bb5
chengw12317 <1183313963@qq.com>
2021-06-22 23:32:08 +0800

    jenkins-test

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by chenguangwei on 2021/6/20.
+ */
+public class LoginTest {
+    @Test
+    public void loginSuccess(){
+        System.out.println("登录成功");
+    }
+}
---
+++ src/test/java/test/RegisterTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by chenguangwei on 2021/6/20.
+ */
+public class RegisterTest {
+    @Test
+    public void loginsuccess(){
+        System.out.println("注册成功");
+    }
+}
